6b7764b70e2e0f512bac872516cca50ab56bf0fc
Kimberly Herrera <kimherrera601@gmail.com>
2021-09-27 10:49:29 -0600

    MVP

--- Multiply/ViewController.swift
+++ Multiply/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    // Variable Declerations
+    
+    @IBOutlet weak var textField1: UITextField!
+    
+    @IBOutlet weak var textField2: UITextField!
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBOutlet weak var multiplyLable: UILabel!
+    
+//Action
+    @IBAction func calculate(_ sender: Any)
+    {
+        var x = textField1.text ?? "0"
+        var y = textField2.text ?? "0"
+        var intgerNumber1 = Int(x) ?? 0
+        var integerNumber2 = Int(y) ?? 0
+    }
+    
 }
 
